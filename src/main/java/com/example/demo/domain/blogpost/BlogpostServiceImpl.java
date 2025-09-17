8dcdc1dc57170acb3a4128dfd746e9fab23bd7c6
Azar <azar.wirth@outlook.com>
2025-09-17 16:28:58 +0200

    created base functions for read

--- src/main/java/com/example/demo/domain/blogpost/BlogpostServiceImpl.java
+++ src/main/java/com/example/demo/domain/blogpost/BlogpostServiceImpl.java
@@ -1,7 +1,38 @@
 package com.example.demo.domain.blogpost;
 
 import com.example.demo.core.generic.AbstractServiceImpl;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.UUID;
 
+@Log4j2
 @Service
 public class BlogpostServiceImpl extends AbstractServiceImpl<Blogpost> implements BlogpostService {
+
+
+    private final BlogpostRepository blogpostRepository;
+
+    public BlogpostServiceImpl(BlogpostRepository blogpostRepository) {
+        this.blogpostRepository = blogpostRepository;
+    }
+
+    public List<Blogpost> findAllBlogposts(){
+        List<Blogpost> post = repository.findAll();
+        if (post.isEmpty()){
+            log.warn("No blogposts found in the database");
+        } else {
+            log.info(post.size() + " blogposts retrieved from database.");
+        }
+        return post;
+    }
+
+    public Blogpost findBlogpostById(UUID id){
+        return blogpostRepository.findById(id)
+                .orElseThrow(() -> {
+                    log.warn("Blogpost with ID {} not found.", id);
+                    return new RuntimeException("Blogpost not found");
+                });
+
+    }
 }
