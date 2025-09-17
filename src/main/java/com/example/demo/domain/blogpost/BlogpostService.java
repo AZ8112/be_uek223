e61af31415dc587cc92f982d99243689884704f9
Leon Wulff <leon.wulff@edu.tbz.ch>
2025-09-17 15:53:14 +0200

    add deleteBlogpost in BlogpostService

--- src/main/java/com/example/demo/domain/blogpost/BlogpostService.java
+++ src/main/java/com/example/demo/domain/blogpost/BlogpostService.java
@@ -1,6 +1,7 @@
 package com.example.demo.domain.blogpost;
 
 import com.example.demo.core.generic.AbstractService;
+import jakarta.persistence.EntityNotFoundException;
 
 import java.util.NoSuchElementException;
 
@@ -17,4 +18,13 @@ public interface BlogpostService extends AbstractService<Blogpost> {
      */
     Blogpost updateBlogpost(String id, Blogpost blogpost)
             throws NoSuchElementException, IllegalArgumentException;
+
+    /**
+     * delete a blogpost by its id
+     *
+     * @param id of blogpost
+     * @throws EntityNotFoundException if blogpost not found
+     * @see Blogpost
+     */
+    void deleteBlogpost(String id) throws EntityNotFoundException;
 }
