481699c79e13c50dee4e2502308bb6f51722e6d4
Leon Wulff <leon.wulff@edu.tbz.ch>
2025-09-17 16:20:05 +0200

    add deleteBlogpost method in BlogpostServiceImpl

--- src/main/java/com/example/demo/domain/blogpost/BlogpostServiceImpl.java
+++ src/main/java/com/example/demo/domain/blogpost/BlogpostServiceImpl.java
@@ -9,13 +9,6 @@ import java.util.*;
 @Service
 public class BlogpostServiceImpl extends AbstractServiceImpl<Blogpost> implements BlogpostService {
 
-    private final BlogpostService blogpostService;
-
-    public BlogpostServiceImpl(BlogpostRepository repository, BlogpostService userService) {
-        super(repository);
-        this.blogpostService = userService;
-    }
-
     /**
      * {@inheritDoc}
      */
@@ -27,11 +20,23 @@ public class BlogpostServiceImpl extends AbstractServiceImpl<Blogpost> implement
         }
 
         Blogpost existingBlogpost = repository.findById(id)
-                .orElseThrow(() -> new NoSuchElementException(String.format("Company with id: '%s' was not found", id)));
+                .orElseThrow(() -> new NoSuchElementException(String.format("Blogpost with id: '%s' was not found", id)));
         existingBlogpost.setTitle(blogpost.getTitle());
         existingBlogpost.setText(blogpost.getText());
         existingBlogpost.setAuthor(blogpost.getAuthor());
         existingBlogpost.setCreatedAt(blogpost.getCreatedAt());
         return repository.save(existingBlogpost);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void deleteBlogpost(UUID id) throws EntityNotFoundException {
+        if (!repository.existsById(id)) {
+            throw new EntityNotFoundException(String.format("Blogpost with id: %s doesn't exist", id));
+        } else {
+            repository.deleteById(id);
+        }
     }
 }
