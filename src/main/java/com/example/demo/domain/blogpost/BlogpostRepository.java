c0ed415fb48a9dfdfab0fb6d093cf4e818b4b398
Azar <azar.wirth@outlook.com>
2025-09-18 09:59:04 +0200

    added find by authorId in repo

--- src/main/java/com/example/demo/domain/blogpost/BlogpostRepository.java
+++ src/main/java/com/example/demo/domain/blogpost/BlogpostRepository.java
@@ -3,6 +3,10 @@ package com.example.demo.domain.blogpost;
 import com.example.demo.core.generic.AbstractRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.UUID;
+
 @Repository
 public interface BlogpostRepository extends AbstractRepository<Blogpost> {
+    List<Blogpost> findByAuthor(UUID authorId);
 }
