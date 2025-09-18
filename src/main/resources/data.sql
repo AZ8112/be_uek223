76be93a51dac01ab4c07325b5c78945dbc4d60a8
Mert Bal <mert.bal@edu.tbz.ch>
2025-09-18 09:33:13 +0200

    Create new authorities and assign them to roles

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -20,7 +20,11 @@ ON CONFLICT DO NOTHING;
 INSERT INTO authority(id, name)
 VALUES ('2ebf301e-6c61-4076-98e3-2a38b31daf86', 'DEFAULT'),
 ('76d2cbf6-5845-470e-ad5f-2edb9e09a868', 'USER_MODIFY'),
-('21c942db-a275-43f8-bdd6-d048c21bf5ab', 'USER_DEACTIVATE')
+('21c942db-a275-43f8-bdd6-d048c21bf5ab', 'USER_DEACTIVATE'),
+('242ff7f7-7e6d-41eb-b52a-59b2e12c5189', 'READ_BLOGPOST'),
+('cc9c3170-b2dc-4701-806a-998b74f23650', 'CREATE_BLOGPOST'),
+('38296949-7a94-4827-9af5-6d381fd1a507', 'UPDATE_BLOGPOST'),
+('a60481f1-8413-4789-9340-572827a140e5', 'DELETE_BLOGPOST'),
 ON CONFLICT DO NOTHING;
 
 --assign roles to users
@@ -35,5 +39,15 @@ values ('ba804cb9-fa14-42a5-afaf-be488742fc54', 'd29e709c-0ff1-4f4c-a7ef-09f656c
 INSERT INTO role_authority(role_id, authority_id)
 VALUES ('d29e709c-0ff1-4f4c-a7ef-09f656c390f1', '2ebf301e-6c61-4076-98e3-2a38b31daf86'),
 ('ab505c92-7280-49fd-a7de-258e618df074', '76d2cbf6-5845-470e-ad5f-2edb9e09a868'),
-('c6aee32d-8c35-4481-8b3e-a876a39b0c02', '21c942db-a275-43f8-bdd6-d048c21bf5ab')
+('c6aee32d-8c35-4481-8b3e-a876a39b0c02', '21c942db-a275-43f8-bdd6-d048c21bf5ab'),
+-- Blogpost authorities for admin
+('ab505c92-7280-49fd-a7de-258e618df074', '242ff7f7-7e6d-41eb-b52a-59b2e12c5189'),
+('ab505c92-7280-49fd-a7de-258e618df074', 'cc9c3170-b2dc-4701-806a-998b74f23650'),
+('ab505c92-7280-49fd-a7de-258e618df074', '38296949-7a94-4827-9af5-6d381fd1a507'),
+('ab505c92-7280-49fd-a7de-258e618df074', 'a60481f1-8413-4789-9340-572827a140e5'),
+-- Blogpost authorities for user
+('c6aee32d-8c35-4481-8b3e-a876a39b0c02', '242ff7f7-7e6d-41eb-b52a-59b2e12c5189'),
+('c6aee32d-8c35-4481-8b3e-a876a39b0c02', 'cc9c3170-b2dc-4701-806a-998b74f23650'),
+('c6aee32d-8c35-4481-8b3e-a876a39b0c02', '38296949-7a94-4827-9af5-6d381fd1a507'),
+('c6aee32d-8c35-4481-8b3e-a876a39b0c02', 'a60481f1-8413-4789-9340-572827a140e5'),
  ON CONFLICT DO NOTHING;
