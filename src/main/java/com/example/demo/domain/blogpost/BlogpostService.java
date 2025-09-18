b85025befeab5b8ce961551fb8bf6fb7ddceba54
Azar <azar.wirth@outlook.com>
2025-09-18 09:28:47 +0200

    added methods to interface

--- src/main/java/com/example/demo/domain/blogpost/BlogpostService.java
+++ src/main/java/com/example/demo/domain/blogpost/BlogpostService.java
@@ -2,6 +2,12 @@ package com.example.demo.domain.blogpost;
 
 import com.example.demo.core.generic.AbstractService;
 
+import java.util.List;
+import java.util.UUID;
+
 public interface BlogpostService extends AbstractService<Blogpost> {
 
+    List<Blogpost> findAllBlogposts();
+
+    Blogpost findBlogpostById(UUID id);
 }
